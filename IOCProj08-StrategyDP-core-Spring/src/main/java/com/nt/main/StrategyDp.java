470336300115356a4e6ae186ba81ebb0996a5439
madithapimahendra <madithapimahendra@gamil.com>
2025-11-12 12:04:39 +0530

    3 change

--- IOCProj08-StrategyDP-core-Spring/src/main/java/com/nt/main/StrategyDp.java
+++ IOCProj08-StrategyDP-core-Spring/src/main/java/com/nt/main/StrategyDp.java
@@ -12,7 +12,7 @@ public static void main(String[] args) {
 Flipkart fpkt =ctx.getBean("fpkt",Flipkart.class);
 System.out.println(" iam the delivered your order at hyd pls check ");
 
-String msg=fpkt.Order(100, 3000);
+String msg=fpkt.Order(1000, 3000);
 System.out.println(msg);
 	}
 	catch(Exception e) 
