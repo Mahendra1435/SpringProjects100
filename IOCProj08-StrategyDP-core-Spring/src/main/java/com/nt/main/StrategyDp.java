9a96c88224acffc52629b128f59fa51a41ce4fca
madithapimahendra <madithapimahendra@gamil.com>
2025-11-12 11:55:58 +0530

    2 change

--- IOCProj08-StrategyDP-core-Spring/src/main/java/com/nt/main/StrategyDp.java
+++ IOCProj08-StrategyDP-core-Spring/src/main/java/com/nt/main/StrategyDp.java
@@ -10,6 +10,7 @@ public static void main(String[] args) {
 	try(AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext(Appconfig.class);)
 	{
 Flipkart fpkt =ctx.getBean("fpkt",Flipkart.class);
+System.out.println(" iam the delivered your order at hyd pls check ");
 
 String msg=fpkt.Order(100, 3000);
 System.out.println(msg);
